ae4855e4c68bfe0896e797e1543a6a155037ac1f
feifuzeng <feifz@tydic.com>
2019-04-15 17:33:07 +0800

    add design pattern @strategy

---
+++ designpattern-demo/src/main/java/com/ohaotian/feifz/designpattern/strategy/Strategy.java
@@ -0,0 +1,11 @@
+package com.ohaotian.feifz.designpattern.strategy;
+
+/**
+ * @author feifz
+ * @version 1.0.0
+ * @Description 策略接口定义
+ * @createTime 2019年04月15日 17:23:00
+ */
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
---
+++ designpattern-demo/src/main/java/com/ohaotian/feifz/designpattern/strategy/impl/OperationAdd.java
@@ -0,0 +1,16 @@
+package com.ohaotian.feifz.designpattern.strategy.impl;
+
+import com.ohaotian.feifz.designpattern.strategy.Strategy;
+
+/**
+ * @author feifz
+ * @version 1.0.0
+ * @Description 策略接口实现-加法
+ * @createTime 2019年04月15日 17:24:00
+ */
+public class OperationAdd implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
